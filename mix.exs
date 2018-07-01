0ddd6c1fdcf6a2295517079b5a57222fc92dfe3c
Leonardo Tegon <ltegon93@gmail.com>
2018-07-01 10:45:09 -0300

    Create top level application

--- mix.exs
+++ mix.exs
@@ -14,15 +14,13 @@ defmodule Todo.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {Todo.Application, []}
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-    ]
+    [{:meck, "0.8.2", only: :test}]
   end
 end
